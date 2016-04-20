bdacfa1d1e7d8cc263b2b2a7b1fc16eaafeab441
Krystian Sobolewski <kleophass@gmail.com>
2016-04-20 12:40:35 +0200

    Wstepne utworzenie PlayerInput, cofniecie zmian z PlayerController

--- Assets/src/PlayerController.cs
+++ Assets/src/PlayerController.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Kontroluje zachowanie sie objektu gracze na scenie
 public class PlayerController : MonoBehaviour {
+    
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
 }
--- Assets/src/PlayerInput.cs
+++ Assets/src/PlayerInput.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Klasa odpowiedzialna za pobieranie i przetwarzanie informacji z wejścia, dotyczacych postaci gracza
+
+[RequireComponent (typeof (PlayerController))] //wymaga komponentu PlayerController w obiekcie
 public class PlayerInput : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    Vector3 moveInput;
+    Vector3 moveVelocity;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +17,6 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 	}
 }
