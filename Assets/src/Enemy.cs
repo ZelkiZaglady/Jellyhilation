b30b21863406994edfe993dd8511507aeae061bf
Aszymczak <a.szymczak-lysiak@student.po.edu.pl>
2016-04-29 15:10:38 +0200

    Enemy class

---
+++ Assets/src/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    private int _hp; //ilość życia
+    private int _hud; //szybkość porrusznia się
+    private int _fireSpeed; //szybkość strzelania
+    private int _hurt; //rany jakie zadaje
+    Enemy(int hp, int hud, int fireSpeed, int hurt)
+    {
+        _hp = hp;
+        _hud = hud;
+        _fireSpeed = fireSpeed;
+        _hurt = hurt;
+    }
+}
